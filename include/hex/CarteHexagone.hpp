e42d7238a754555b56d3f0f67cd29f44a0ba23b9
Florian Cormée <florian.cormee@gmail.com>
2020-05-26 18:31:50 +0200

    Fix, nettoyage et commentaire.

--- include/hex/CarteHexagone.hpp
+++ include/hex/CarteHexagone.hpp
@@ -20,32 +20,91 @@ namespace hex
         T** m_plateau;
 
     public:
+        /**
+         * @brief Construit une nouvelle carte hexagonale.
+         *
+         * Le rayon ne tient pas compte de la case centrale. Un masque de rayon
+         * 0 est de diamètre 1. Un masque de rayon 2 est de diamètre 5.
+         *
+         * @param rayon Le rayon de l'hexagone (ne compte pas le centre).
+         */
         explicit CarteHexagone(size_t rayon) : m_rayon(rayon), m_plateau(nullptr)
         {
+            // Crée le plateau puis chaque ligne de longueur croissante et décroissante.
             size_t diametre = 2 * m_rayon + 1;
             m_plateau = new T*[diametre];
             for (size_t i = 1; i <= m_rayon; i++)
             {
-                m_plateau[m_rayon + i] = new T[diametre - i];
-                m_plateau[m_rayon - i] = new T[diametre - i];
+                size_t longueur = diametre - i;
+                m_plateau[m_rayon + i] = new T[longueur];
+                m_plateau[m_rayon - i] = new T[longueur];
             }
+            // Crée la ligne centrale.
             m_plateau[m_rayon] = new T[diametre];
         }
 
-        virtual ~CarteHexagone()
+        /*====== Constructeurs & opérateurs de copie & destructeur ======*/
+        CarteHexagone(const CarteHexagone& autre) : m_rayon(0), m_plateau(nullptr) { *this = autre; }
+
+        CarteHexagone(CarteHexagone&& autre) : m_rayon(0), m_plateau(nullptr) { *this = autre; }
+
+        CarteHexagone& operator=(const CarteHexagone& autre)
         {
+            deletePlateau();
+            // Copie les valeurs depuis autre vers l'instance.
+            m_rayon = autre.m_rayon;
             size_t diametre = 2 * m_rayon + 1;
-            for (size_t i = 0; i < diametre; i++)
+            m_plateau = new T*[diametre];
+            // Copie les lignes de taille croissante et décroissante.
+            for (size_t i = 1; i <= m_rayon; i++)
             {
-                delete[] m_plateau[i];
+                size_t longueur = diametre - i;
+                m_plateau[m_rayon + i] = new T[longueur];
+                T* source = autre.m_plateau[m_rayon + i];
+                T* destination = m_plateau[m_rayon + i];
+                std::copy(source, source + longueur, destination);
+
+                m_plateau[m_rayon - i] = new T[longueur];
+                source = autre.m_plateau[m_rayon - i];
+                destination = m_plateau[m_rayon - i];
+                std::copy(source, source + longueur, destination);
             }
-            delete[] m_plateau;
+            // Copie la ligne centrale.
+            m_plateau[m_rayon] = new T[diametre];
+            T* source = autre.m_plateau[m_rayon];
+            std::copy(source, source + diametre, m_plateau[m_rayon]);
+
+            return *this;
         }
 
+        CarteHexagone& operator=(CarteHexagone&& autre)
+        {
+            deletePlateau();
+            // Mets le pointeur d'autre à la place de celui de l'instance et met
+            // à nullptr celui d'autre. Similaire pour les rayons.
+            m_plateau = autre.m_plateau;
+            autre.m_plateau = nullptr;
+            m_rayon = autre.m_rayon;
+            autre.m_rayon = 0;
+            return *this;
+        }
+
+        virtual ~CarteHexagone() { deletePlateau(); }
+
+        /*====== Méthodes ======*/
+
+        /**
+         * @brief Obtient le rayon.
+         *
+         * Le rayon ne tient pas compte de la case centrale.
+         *
+         * @return Le rayon.
+         */
         size_t getRayon() const { return m_rayon; }
 
         virtual T& operator()(const Coordonnees& c) override
         {
+            // Vérifie que les coordonnees sont dans l'hexagone.
             if (abs(c.getLigne()) > (int)m_rayon || abs(c.getColonne()) > (int)m_rayon)
             {
                 std::stringstream ss;
@@ -58,6 +117,7 @@ namespace hex
 
         virtual const T& operator()(const Coordonnees& c) const override
         {
+            // Vérifie que les coordonnees sont dans l'hexagone.
             if (abs(c.getLigne()) > (int)m_rayon || abs(c.getColonne()) > (int)m_rayon)
             {
                 std::stringstream ss;
@@ -67,9 +127,11 @@ namespace hex
             return m_plateau[m_rayon - c.getColonne()][m_rayon - c.getLigne()];
         }
 
-        virtual void remplir(const T& valeur) override {
+        virtual void remplir(const T& valeur) override
+        {
+            // On itère sur chaque case et on lui alloue sa nouvelle valeur.
             auto itr = iterateur();
-            while(itr->aSuite())
+            while (itr->aSuite())
             {
                 Coordonnees cCase = itr->suite();
                 (*this)(cCase) = valeur;
@@ -78,11 +140,14 @@ namespace hex
 
         virtual void remplir(const ICarte<T>& source, const Coordonnees& centre) override
         {
+            // On itère sur chaque case de la source.
             auto iterateur = source.iterateur();
             while (iterateur->aSuite())
             {
+                // On calcul les coordonnees de destination (avec un offset du centre).
                 Coordonnees cSource = iterateur->suite();
                 Coordonnees cThis = cSource + centre;
+                // On définit la nouvelle valeur si les coordonnees après offset sont valides.
                 try
                 {
                     (*this)(cThis) = source(cSource);
@@ -96,10 +161,27 @@ namespace hex
 
         virtual IIterator_S<Coordonnees> iterateur() const override
         {
+            // Crée un itérateur pour l'instance et le retourne.
             IIterator_S<Coordonnees> itr(new IterateurCarteHexagone<T>(this));
             return itr;
         }
-    };
 
+    protected:
+        /**
+         * @brief Libère la mémoire allouée.
+         */
+        void deletePlateau()
+        {
+            if (m_plateau != nullptr)
+            {
+                size_t taille = 2 * m_rayon + 1;
+                for (size_t i = 0; i < taille; i++)
+                {
+                    delete[] m_plateau[i];
+                }
+                delete[] m_plateau;
+            }
+        }
+    };
 }; // namespace hex
 #endif
--- test/hex/MasqueTest.cpp
+++ test/hex/MasqueTest.cpp
@@ -1,11 +1,11 @@
-#include "catch.hpp"
 #include "hex/Masque.hpp"
+#include "catch.hpp"
 #include "hex/CarteHexagone.hpp"
 #include "hex/Coordonnees.hpp"
 
 using namespace hex;
 
-TEST_CASE( "Creation et egalite de masque", "[masque]" )
+TEST_CASE("Creation et egalite de masque", "[masque]")
 {
     Masque m1(2);
     m1.remplir(true);
@@ -15,21 +15,21 @@ TEST_CASE( "Creation et egalite de masque", "[masque]" )
     REQUIRE(m1(Coordonnees()));
     REQUIRE_FALSE(m2(Coordonnees()));
 
-    SECTION( "operator||" )
+    SECTION("operator||")
     {
-        Masque m3(m1||m2);
+        Masque m3(m1 || m2);
         REQUIRE(m3(Coordonnees()));
     }
 
-    SECTION( "operator&&" )
+    SECTION("operator&&")
     {
-        Masque m3(m1&&m2);
+        Masque m3(m1 && m2);
         REQUIRE_FALSE(m3(Coordonnees()));
     }
 
-    SECTION( "operator!" )
+    SECTION("operator!")
     {
         m1 = !m1;
-        REQUIRE_FALSE(!m1(Coordonnees()));
+        REQUIRE_FALSE(m1(Coordonnees()));
     }
 }
