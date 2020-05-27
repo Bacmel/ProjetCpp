791a40323338a6ff72b52a82c723c0321e343535
Bacmel <hugo.duarte91800@gmail.com>
2020-05-27 16:41:29 +0200

    Création Jauge

---
+++ src/utils/Jauge.cpp
@@ -0,0 +1,34 @@
+#include "utils/Jauge.hpp"
+#include "err/JaugeErreur.hpp"
+
+namespace utils
+{
+    void Jauge::ajouterValeur(int valeur)
+    {
+        if ((int)m_val < -valeur) // Throw
+        {
+            throw err::JaugeErreur("Jauge::ajouteValeur : m_val negatif");
+        }
+        else if (m_val + valeur < m_valMax)
+        {
+            m_val += valeur;
+        }
+        else
+        {
+            m_val = m_valMax;
+        }
+    }
+
+    void Jauge::ajouterValeurMax(int valeur)
+    {
+        if ((int)m_valMax < -valeur) // Throw
+        {
+            throw err::JaugeErreur("Jauge::ajouteValeurMax : m_valMax negatif");
+        }
+        else
+        {
+            m_valMax += valeur;
+            ajouterValeur(valeur);
+        }
+    }
+} // namespace utils
