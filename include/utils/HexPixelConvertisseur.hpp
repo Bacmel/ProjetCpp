cfde5f2eafd4af3d31082a454754a5e91f4942bc
Florian Cormée <florian.cormee@gmail.com>
2020-05-29 15:18:30 +0200

    Ajout HexPixelConvertisseur

---
+++ include/utils/HexPixelConvertisseur.hpp
@@ -0,0 +1,31 @@
+#ifndef __HEXPIXELCONVERTISSEUR_H__
+#define __HEXPIXELCONVERTISSEUR_H__
+
+#include <SFML/Graphics.hpp>
+#include "hex/Coordonnees.hpp"
+
+namespace utils
+{
+    class HexPixelConvertisseur
+    {
+    public:
+        /**
+         * @brief Converti la coordonnees hexagonale en position en pixel.
+         *
+         * @param cote La longueur du cote des hexagones.
+         * @param pixel La position en grille hexagonale.
+         * @return L'équivalent en matrice de pixels.
+         */
+        sf::Vector2f operator()(float cote, const hex::Coordonnees& hex) const;
+
+        /**
+         * @brief Converti la position en pixel en une coordonnees hexagonale.
+         *
+         * @param cote La longueur du cote des hexagones.
+         * @param pixel La position en pixel.
+         * @return L'équivalent en grille hexagonale.
+         */
+        hex::Coordonnees operator()(float cote, const sf::Vector2u& pixel) const;
+    };
+} // namespace utils
+#endif // __HEXPIXELCONVERTISSEUR_H__
---
+++ src/utils/HexPixelConvertisseur.cpp
@@ -0,0 +1,22 @@
+#include "utils/HexPixelConvertisseur.hpp"
+#include <cmath>
+
+namespace utils
+{
+    sf::Vector2f HexPixelConvertisseur::operator()(float cote, const hex::Coordonnees& hex) const
+    {
+        sf::Vector2f pixel;
+        pixel.x = cote * (float)hex.getColonne() * 3. / 2.;
+        pixel.y = cote * (hex.getColonne() / 2. + hex.getLigne()) * sqrtf(3);
+        return pixel;
+    }
+
+    hex::Coordonnees HexPixelConvertisseur::operator()(float cote, const sf::Vector2u& pixel) const
+    {
+        float q = pixel.x / cote * 2. / 3.;
+        float r = (sqrtf(3) * pixel.y - pixel.x) / 3.;
+        hex::Coordonnees hex;
+        hex.arrondir(r, q);
+        return hex;
+    }
+} // namespace utils
