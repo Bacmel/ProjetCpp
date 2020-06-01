f3085501da5b651aa96166d5b3d26fd43dcc7af2
Florian Cormée <florian.cormee@gmail.com>
2020-06-01 16:40:55 +0200

    Dessin de l'inventaire.

--- include/vue/PartieDessinable.hpp
+++ include/vue/PartieDessinable.hpp
@@ -12,6 +12,9 @@ namespace vue
     {
     private:
         utils::HexPixelConvertisseur m_convertisseur;
+        sf::Font m_font;
+        sf::Text m_equipeText;
+        sf::RectangleShape m_equipeIndicateur;
 
     public:
         PartieDessinable(float cote);
@@ -22,9 +25,13 @@ namespace vue
 
         void draw(sf::RenderTarget& target, sf::RenderStates states) const override;
 
+        sf::FloatRect getCaseInventaire(size_t indice) const;
+
     private:
         void drawCarte(sf::RenderTarget& target, sf::RenderStates& states, donjon::IDonjon& donjon) const;
         void drawPersonnages(sf::RenderTarget& target, sf::RenderStates& states, donjon::IDonjon& donjon) const;
+        void drawEquipe(sf::RenderTarget& target, sf::RenderStates& states) const;
+        void drawInventaire(sf::RenderTarget& target, sf::RenderStates& states) const;
         sf::Color couleurEquipe(size_t equipe) const;
     };
 } // namespace vue
