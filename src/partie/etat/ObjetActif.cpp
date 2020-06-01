9f8bf5ee9da365706ad0ce03aedc512dfe759be0
Florian Cormée <florian.cormee@gmail.com>
2020-06-01 17:58:10 +0200

    Implémentation partielle d'ObjetActif

---
+++ src/partie/etat/ObjetActif.cpp
@@ -0,0 +1,9 @@
+#include "partie/etat/ObjetActif.hpp"
+
+namespace partie::etat
+{
+    void ObjetActif::operation(Partie& partie, const hex::Coordonnees& coordonnees) {
+
+    }
+
+} // namespace partie::etat
