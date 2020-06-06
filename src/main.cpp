4c24ab5ea922496c737f0c61e714453233d243fd
Florian Cormée <florian.cormee@gmail.com>
2020-06-06 19:15:37 +0200

    Seed random au démarrage.

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,10 @@
 #include "Roblite.hpp"
+#include <cstdlib>
+#include <ctime>
 
 int main()
 {
+    srand(time(NULL));
     // Création de la classe de plus haut niveau et lancement du jeu.
     Roblite roblite;
     roblite.jouer();
